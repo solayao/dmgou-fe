d689499ed70ab9ca4b4d4f5c5d8dcbe6c9fd8b16
liuchengbiao@gosuncn.com <liuchengbiao@gosuncn.com>
2018-12-19 16:19:06 +0800

    上线处理

--- src/query/socket.js
+++ src/query/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 let socket = null;
 const createSocket = () => {
     if (!socket) {
-        socket = io(process.env.REACT_APP_SOCKETIO_PATH);
+        socket = io();
     }
     return socket;
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/graphql', { target: 'http://localhost:4627' }));
-  app.use(proxy('/getImg', { target: 'http://localhost:4627' }));
+  app.use(proxy('/graphql', { target: 'http://localhost:4628' }));
+  app.use(proxy('/getImg', { target: 'http://localhost:4628' }));
+  app.use(proxy('/socket.io', { target: 'http://localhost:4627' }));
 };

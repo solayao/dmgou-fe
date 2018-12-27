d8e3bb9a67b643d7cd62a8a31905ae0a953c2b72
liuchengbiao@gosuncn.com <liuchengbiao@gosuncn.com>
2018-12-27 15:51:47 +0800

    upgrand

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,5 +3,8 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(proxy('/graphql', { target: 'http://localhost:4628' }));
   app.use(proxy('/getImg', { target: 'http://localhost:4628' }));
-  app.use(proxy('/socket.io', { target: 'http://localhost:4627' }));
+  app.use(proxy('/socket.io', { 
+    target: 'http://localhost:4627',
+    ws:true
+  }));
 };
